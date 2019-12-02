d4d5ba87460e70f81c25bfd7195e797f9342a3d0
bitlischieber <bitlischieber@users.noreply.github.com>
2019-12-03 00:05:37 +0100

    Day 2 added
    Solution of day 2 in C#

---
+++ D2/Program_p1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inpCodeStr = System.IO.File.ReadAllText(@".\..\..\input.txt");
+            int[] inpCode = inpCodeStr.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+
+            CompIntcode(ref inpCode, 12, 2);
+
+            for (int i = 0; i < inpCode.Length; i++)
+            {
+                Console.WriteLine("{0:00}: {1}", i, inpCode[i]);
+            }
+            Console.ReadLine();
+
+            // Initial value on pos. 0: 250673
+            // with chagnged values at start: 5110675
+        }
+
+        private static void CompIntcode(ref int[] inpCode, int noun, int verb)
+        {
+            int pc = 0;
+            // prepate with values to replace
+            inpCode[1] = noun;
+            inpCode[2] = verb;
+            // compute
+            while (true)
+            {
+                if (inpCode[pc] == 99)
+                {
+                    break;
+                }
+                else if (inpCode[pc] == 1)
+                {
+                    // addition
+                    inpCode[inpCode[pc + 3]] = inpCode[inpCode[pc + 1]] + inpCode[inpCode[pc + 2]];
+                }
+                else if (inpCode[pc] == 2)
+                {
+                    // multiplication
+                    inpCode[inpCode[pc + 3]] = inpCode[inpCode[pc + 1]] * inpCode[inpCode[pc + 2]];
+                }
+                pc += 4;
+            }
+        }
+    }
+}
---
+++ D2/Program_p2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inpCodeStr = System.IO.File.ReadAllText(@".\..\..\input.txt");
+            int[] inpCode = inpCodeStr.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+
+            for(int noun = 0; noun < 100; noun++)
+            {
+                for (int verb = 0; verb < 100; verb++)
+                {
+                    int[] tempInp = new int[inpCode.Length];
+                    Array.Copy(inpCode, tempInp, inpCode.Length);
+                    CompIntcode(ref tempInp, noun, verb);
+                    if(tempInp[0] == 19690720)
+                    {
+                        Console.WriteLine("x={0}, y{1} = {2}", noun, verb, tempInp[0]);
+                        Console.WriteLine("Result is: 100 * {0} + {1} = {2}", noun, verb, 100 * noun + verb);
+                    }
+                }
+            }
+
+            Console.WriteLine("END");
+            Console.ReadLine();
+        }
+
+        private static void CompIntcode(ref int[] inpCode, int noun, int verb)
+        {
+            int pc = 0;
+            // prepate with values to replace
+            inpCode[1] = noun;
+            inpCode[2] = verb;
+            // compute
+            try
+            {
+                while (true)
+                {
+                    if (inpCode[pc] == 99)
+                    {
+                        break;
+                    }
+                    else if (inpCode[pc] == 1)
+                    {
+                        // addition
+                        inpCode[inpCode[pc + 3]] = inpCode[inpCode[pc + 1]] + inpCode[inpCode[pc + 2]];
+                    }
+                    else if (inpCode[pc] == 2)
+                    {
+                        // multiplication
+                        inpCode[inpCode[pc + 3]] = inpCode[inpCode[pc + 1]] * inpCode[inpCode[pc + 2]];
+                    }
+                    pc += 4;
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine("{0} and {1} caused an error at pc={2}", noun, verb, pc);
+            }
+        }
+    }
+}
